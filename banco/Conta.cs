b4855a061895404861213cf12bf3d6c3a7875213
carlos Christian <cursosacessiveis202108@gmail.com>
2024-03-25 01:30:56 -0300

    criado construtores, para a classe conta e propriedades auto implementadas.

--- banco/Conta.cs
+++ banco/Conta.cs
@@ -6,5 +6,30 @@ namespace banco
 {
     class Conta
     {
+        public string Titular
+        {
+            get;
+            set
+      ;
+        }
+        public double Saldo
+        {
+            get;
+            private set;
+        }
+        public int NumeroConta
+        {
+            get;
+            private set;
+        }
+        public Conta(string titular, int numeroConta)
+        {
+            Titular = titular;
+            NumeroConta = numeroConta;
+        }
+        public Conta(string titular, int numeroConta, double saldo): this(titular, numeroConta)
+        {
+            Saldo = saldo;
+        }
     }
 }
--- banco/Program.cs
+++ banco/Program.cs
@@ -7,6 +7,5 @@ namespace banco
         static void Main(string[] args)
         {
             Console.WriteLine("Bem-vindo ao banco central.");
-        }
     }
 }
